cc5317c070298d5529a40b8dd683afa81bef37a1
Lindsey Sawatzky <sawatzky.lindsey@gmail.com>
2024-04-27 15:44:18 -0700

    Build out categorical flat rendering.
    Allows for flat rendering of multi-dimensional data (ex: tuples).
    Still planning Api changes.

--- src/lib.rs
+++ src/lib.rs
@@ -1,39 +1,95 @@
-use std::fmt::{Display, Formatter};
+use std::cmp::Ordering;
+use std::collections::{HashMap, HashSet};
+use std::fmt::{Debug, Display, Formatter};
 use std::iter;
+
+#[derive(Debug)]
+pub struct Render {
+    pub render_width: usize,
+    pub show_values: bool,
+}
+
+impl Default for Render {
+    fn default() -> Self {
+        Self {
+            render_width: 120,
+            show_values: false,
+        }
+    }
+}
 
 pub struct Flat {
     rows: Vec<Row>,
-    maximum_value: u32,
-    render_width: u32,
-    show_values: bool,
+    maximum_value: usize,
+    config: Render,
 }
 
 impl Display for Flat {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        let scale: usize = self.maximum_value.div_ceil(self.render_width) as usize;
-        let show: Option<usize> = if self.show_values {
-            Some((self.maximum_value as f64).log10().ceil() as usize)
-        } else {
-            None
-        };
         let mut keys = Vec::with_capacity(self.rows.len());
         let mut key_width = 0;
 
         for row in self.rows.iter() {
-            let key_string = row.render_key();
+            let key = match row.render_key() {
+                Some(key_string) => {
+                    let len = key_string.chars().count();
 
-            if key_width < key_string.len() {
-                key_width = key_string.len();
-            }
+                    if key_width < len {
+                        key_width = len;
+                    }
 
-            keys.push(key_string);
+                    Some(key_string)
+                }
+                None => None,
+            };
+
+            keys.push(key);
         }
 
+        let mut show_width = 0;
+
+        let show: Option<usize> = if self.config.show_values {
+            let width = (self.maximum_value as f64).log10().ceil() as usize;
+            show_width = width + 3;
+            Some(width)
+        } else {
+            None
+        };
+
+        let mut value_width = self
+            .config
+            .render_width
+            .saturating_sub(key_width + 2 + show_width);
+
+        if value_width < 2 {
+            value_width = 2;
+        }
+
+        let scale: usize = self.maximum_value.div_ceil(value_width);
+
         for (i, (row, key)) in self.rows.iter().zip(keys).enumerate() {
-            if i + 1 == self.rows.len() {
-                write!(f, "{key:key_width$}  {}", row.render_value(scale, show))?;
-            } else {
-                write!(f, "{key:key_width$}  {}\n", row.render_value(scale, show))?;
+            match (key, row.render_value(scale, show)) {
+                (Some(key_string), Some(value_string)) => {
+                    if i + 1 == self.rows.len() {
+                        write!(f, "{key_string:key_width$}  {value_string}")?;
+                    } else {
+                        write!(f, "{key_string:key_width$}  {value_string}\n")?;
+                    }
+                }
+                (Some(key_string), None) => {
+                    if i + 1 == self.rows.len() {
+                        write!(f, "{key_string}")?;
+                    } else {
+                        write!(f, "{key_string}\n")?;
+                    }
+                }
+                _ => {
+                    if i + 1 == self.rows.len() {
+                        write!(f, "")?;
+                    } else {
+                        write!(f, "\n")?;
+                    }
+                }
             }
         }
 
@@ -41,41 +97,51 @@ impl Display for Flat {
     }
 }
 
-pub struct Row {
-    key: String,
-    value: u32,
+#[derive(Debug)]
+pub enum Row {
+    Full { key: String, value: usize },
+    Partial { key: String },
+    Empty,
 }
 
 impl Row {
-    fn render_key(&self) -> String {
-        format!("{}", self.key)
+    fn render_key(&self) -> Option<String> {
+        match &self {
+            Row::Full { key, .. } => Some(format!("{}", key)),
+            Row::Partial { key, .. } => Some(format!("{}", key)),
+            Row::Empty => None,
+        }
     }
 
-    fn render_value(&self, scale: usize, show: Option<usize>) -> String {
-        let show = match show {
-            Some(width) => {
-                format!("[{:>width$}] ", self.value)
+    fn render_value(&self, scale: usize, show: Option<usize>) -> Option<String> {
+        match &self {
+            Row::Full { value, .. } => {
+                let show = match show {
+                    Some(width) => {
+                        format!("[{:>width$}] ", value)
+                    }
+                    None => "".to_string(),
+                };
+                Some(format!(
+                    "{}{}",
+                    show,
+                    iter::repeat('*')
+                        .take(value.div_ceil(scale))
+                        .collect::<String>(),
+                ))
             }
-            None => "".to_string(),
-        };
-        format!(
-            "{}{}",
-            show,
-            iter::repeat('*')
-                .take((self.value as usize).div_ceil(scale))
-                .collect::<String>(),
-        )
+            Row::Partial { .. } | Row::Empty => None,
+        }
     }
 }
 
-impl Display for Row {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}  {}", self.key, self.value)
-    }
+pub trait Dimensions: Clone + Ord + Debug {
+    fn locus(&self) -> String;
+
+    fn chain(&self) -> Vec<String>;
 }
 
-pub trait Dimensions {}
-
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub struct OneD<T> {
     a: T,
 }
@@ -86,42 +152,364 @@ impl<T> OneD<T> {
     }
 }
 
-impl<T> Dimensions for OneD<T> {}
-
-pub struct TwoD<T, U> {
-    a: T,
-    b: U,
-}
-
-impl<T, U> TwoD<T, U> {
-    pub fn new(a: T, b: U) -> TwoD<T, U> {
-        Self { a, b }
+impl<T> Display for OneD<T> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "asdf")
     }
 }
 
-impl<T, U> Dimensions for TwoD<T, U> {}
+impl<T: Clone + Display + Ord + Debug> Dimensions for OneD<T> {
+    fn locus(&self) -> String {
+        self.a.to_string()
+    }
+
+    fn chain(&self) -> Vec<String> {
+        vec![self.a.to_string()]
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct TwoD<T, U>
+where
+    T: Ord,
+    U: Ord,
+{
+    primary: T,
+    secondary: U,
+}
+
+impl<T, U> TwoD<T, U>
+where
+    T: Ord,
+    U: Ord,
+{
+    pub fn new(primary: T, secondary: U) -> TwoD<T, U> {
+        Self { primary, secondary }
+    }
+}
+
+impl<T, U> Display for TwoD<T, U>
+where
+    T: Ord,
+    U: Ord,
+{
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "asdf")
+    }
+}
+
+impl<T, U> PartialOrd for TwoD<T, U>
+where
+    T: PartialOrd + Ord,
+    U: PartialOrd + Ord,
+{
+    fn partial_cmp(&self, other: &TwoD<T, U>) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl<T, U> Ord for TwoD<T, U>
+where
+    T: Ord,
+    U: Ord,
+{
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.primary
+            .cmp(&other.primary)
+            .then(self.secondary.cmp(&other.secondary))
+    }
+}
+
+impl<T, U> Dimensions for TwoD<T, U>
+where
+    T: Clone + Display + Ord + Debug,
+    U: Clone + Display + Ord + Debug,
+{
+    fn locus(&self) -> String {
+        self.primary.to_string()
+    }
+
+    fn chain(&self) -> Vec<String> {
+        vec![self.primary.to_string(), self.secondary.to_string()]
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct ThreeD<T, U, V>
+where
+    T: Ord,
+    U: Ord,
+    V: Ord,
+{
+    primary: T,
+    secondary: U,
+    tertiary: V,
+}
+
+impl<T, U, V> ThreeD<T, U, V>
+where
+    T: Ord,
+    U: Ord,
+    V: Ord,
+{
+    pub fn new(primary: T, secondary: U, tertiary: V) -> ThreeD<T, U, V> {
+        Self {
+            primary,
+            secondary,
+            tertiary,
+        }
+    }
+}
+
+impl<T, U, V> Display for ThreeD<T, U, V>
+where
+    T: Ord,
+    U: Ord,
+    V: Ord,
+{
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "asdf")
+    }
+}
+
+impl<T, U, V> PartialOrd for ThreeD<T, U, V>
+where
+    T: PartialOrd + Ord,
+    U: PartialOrd + Ord,
+    V: PartialOrd + Ord,
+{
+    fn partial_cmp(&self, other: &ThreeD<T, U, V>) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl<T, U, V> Ord for ThreeD<T, U, V>
+where
+    T: Ord,
+    U: Ord,
+    V: Ord,
+{
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.primary
+            .cmp(&other.primary)
+            .then(self.secondary.cmp(&other.secondary))
+            .then(self.tertiary.cmp(&other.tertiary))
+    }
+}
+
+impl<T, U, V> Dimensions for ThreeD<T, U, V>
+where
+    T: Clone + Display + Ord + Debug,
+    U: Clone + Display + Ord + Debug,
+    V: Clone + Display + Ord + Debug,
+{
+    fn locus(&self) -> String {
+        self.primary.to_string()
+    }
+
+    fn chain(&self) -> Vec<String> {
+        vec![
+            self.primary.to_string(),
+            self.secondary.to_string(),
+            self.tertiary.to_string(),
+        ]
+    }
+}
 
 pub struct Categorical<D: Dimensions> {
-    data: Vec<(D, u32)>,
+    data: Vec<(D, usize)>,
 }
 
 impl<D: Dimensions> Categorical<D> {
     pub fn builder() -> Categorical<D> {
-        todo!()
+        Self {
+            data: Vec::default(),
+        }
     }
 
-    pub fn add(self, key: D, value: u32) -> Categorical<D> {
+    pub fn add(mut self, key: D, value: usize) -> Categorical<D> {
+        self.data.push((key, value));
         self
     }
 
-    pub fn render(self) -> Flat {
-        todo!()
+    pub fn render(self, config: Render) -> Flat {
+        let mut values: HashMap<String, usize> =
+            self.data.iter().map(|(k, _)| (k.locus(), 0)).collect();
+        let mut full_chains: HashSet<String> = HashSet::default();
+        let mut sort_keys: Vec<D> = Vec::default();
+        let ancestor_key = |chain: &[String], dag_index: usize| chain[0..dag_index + 1].join("|");
+        let mut ancestors: HashMap<String, usize> = HashMap::default();
+        let mut columns = 0;
+        let mut column_widths: HashMap<usize, usize> = HashMap::default();
+        let mut maximum_value: usize = 0;
+
+        for (k, v) in self.data.iter() {
+            let locus = k.locus();
+            let chain = k.chain();
+            assert!(chain[0] == locus);
+            let full_chain = chain.join("|");
+
+            if !full_chains.contains(&full_chain) {
+                full_chains.insert(full_chain);
+
+                if chain.len() > columns {
+                    columns = chain.len();
+                }
+
+                for (dag_index, part) in chain.iter().enumerate() {
+                    let j = chain.len() - dag_index - 1;
+                    let partial = ancestor_key(chain.as_slice(), dag_index);
+                    ancestors
+                        .entry(partial)
+                        .and_modify(|c| *c += 1)
+                        .or_insert(1);
+
+                    let width = part.to_string().len();
+                    let w = column_widths.entry(j).or_default();
+
+                    if width > *w {
+                        *w = width;
+                    }
+                }
+            }
+
+            let aggregate = values
+                .get_mut(&locus)
+                .expect(format!("Key/locus must map to one of the aggregating rows").as_str());
+            *aggregate += v;
+
+            if *aggregate > maximum_value {
+                maximum_value = *aggregate;
+            }
+
+            if !sort_keys.contains(k) {
+                sort_keys.push(k.clone());
+            }
+        }
+
+        sort_keys.sort();
+        let mut rows = Vec::default();
+        let mut column_groups: HashMap<usize, Group> = HashMap::default();
+        let mut sep = "ᗒ";
+        let mut current_locus = None;
+
+        for k in sort_keys.iter() {
+            let mut group_value = None;
+            let locus = k.locus();
+
+            match &mut current_locus {
+                Some(l) => {
+                    if l != &locus {
+                        current_locus.replace(locus.clone());
+
+                        if sep == "ᗒ" {
+                            sep = "ᐅ";
+                        } else {
+                            sep = "ᗒ";
+                        }
+                    }
+                }
+                None => {
+                    current_locus = Some(locus.clone());
+                }
+            }
+
+            let chain = k.chain();
+            let mut key = "".to_string();
+
+            for (j, part) in chain.iter().rev().enumerate() {
+                let dag_index = chain.len() - j - 1;
+                let width = column_widths[&j];
+                let partial = ancestor_key(chain.as_slice(), dag_index);
+
+                let group = column_groups.entry(j).or_default();
+
+                if group.matches(&partial) {
+                    group.increment();
+                } else {
+                    group.swap(partial.clone());
+                }
+
+                let count = ancestors[&partial];
+
+                if group.index == (count as f64 / 2.0).ceil() as usize - 1 {
+                    if key.is_empty() {
+                        key += format!("{part:width$} {sep}").as_str();
+                    } else {
+                        key += format!(" {part:width$} {sep}").as_str();
+                    }
+
+                    if dag_index == 0 {
+                        group_value = Some(*values.get(&locus).unwrap());
+                    }
+                } else {
+                    if key.is_empty() {
+                        key += format!("{:width$}", "").as_str();
+                    } else {
+                        key += format!(" {:width$}", "").as_str();
+                    }
+                }
+            }
+
+            match group_value {
+                Some(value) => {
+                    rows.push(Row::Full {
+                        key: key.trim().to_string(),
+                        value,
+                    });
+                }
+                None => {
+                    rows.push(Row::Partial {
+                        key: key.trim().to_string(),
+                    });
+                }
+            }
+        }
+
+        Flat {
+            rows,
+            maximum_value,
+            config,
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Group {
+    locus: Option<String>,
+    index: usize,
+}
+
+impl Default for Group {
+    fn default() -> Self {
+        Self {
+            locus: None,
+            index: 0,
+        }
+    }
+}
+
+impl Group {
+    fn matches(&self, locus: &String) -> bool {
+        match &self.locus {
+            Some(l) => l == locus,
+            None => false,
+        }
+    }
+
+    fn swap(&mut self, locus: String) {
+        self.locus.replace(locus);
+        self.index = 0;
+    }
+
+    fn increment(&mut self) {
+        self.index += 1;
     }
 }
 
 pub struct Histogram {
     bins: usize,
-    data: Vec<(f64, u32)>,
+    data: Vec<(f64, usize)>,
     min: f64,
     max: f64,
 }
@@ -136,7 +524,7 @@ impl Histogram {
         }
     }
 
-    pub fn add(mut self, key: f64, value: u32) -> Histogram {
+    pub fn add(mut self, key: f64, value: usize) -> Histogram {
         if key < self.min {
             self.min = key;
         }
@@ -149,12 +537,12 @@ impl Histogram {
         self
     }
 
-    pub fn render(self, show_values: bool) -> Flat {
+    pub fn render(self, config: Render) -> Flat {
         let Self {
             bins,
-            mut data,
-            mut min,
-            mut max,
+            data,
+            min,
+            max,
         } = self;
 
         let delta = max - min;
@@ -174,15 +562,16 @@ impl Histogram {
                 }
             })
             .collect();
-        let mut values: Vec<u32> = (0..bins).map(|_| 0).collect();
-        let mut maximum_value: u32 = 0;
+        let mut values: Vec<usize> = (0..bins).map(|_| 0).collect();
+        let mut maximum_value: usize = 0;
 
         for (k, v) in data {
+            // TODO: Fix for performance
             let (index, _) = bin_ranges
                 .iter()
                 .enumerate()
                 .find(|(_, r)| r.contains(&k))
-                .expect(format!("Key '{k}' must map to one of the bins").as_str());
+                .expect(format!("Key '{k}' must map to one of the aggregating bins").as_str());
             values[index] += v;
 
             if values[index] > maximum_value {
@@ -194,14 +583,13 @@ impl Histogram {
             rows: bin_ranges
                 .into_iter()
                 .zip(values)
-                .map(|(bounds, value)| Row {
+                .map(|(bounds, value)| Row::Full {
                     key: bounds.to_string(),
-                    value,
+                    value: value,
                 })
                 .collect(),
             maximum_value,
-            render_width: 20,
-            show_values,
+            config,
         }
     }
 }
--- tests/main.rs
+++ tests/main.rs
@@ -1,20 +1,66 @@
 use flat::*;
+use rstest::rstest;
 
 #[test]
-fn categorical() {
-    let builder = Categorical::builder();
-    let builder = builder
-        .add(TwoD::new("abc".to_string(), 4u32), 1)
-        .add(TwoD::new("abc".to_string(), 3u32), 2)
-        .add(TwoD::new("def".to_string(), 4u32), 3);
-    let flat = builder.render();
+fn categorical_1d() {
+    let builder = Categorical::builder()
+        .add(OneD::new("whale".to_string()), 0)
+        .add(OneD::new("shark".to_string()), 1)
+        .add(OneD::new("shark".to_string()), 3)
+        .add(OneD::new("tiger".to_string()), 1)
+        .add(OneD::new("tiger".to_string()), 3)
+        .add(OneD::new("tiger".to_string()), 3);
+    let flat = builder.render(Render::default());
     assert_eq!(
-        flat.to_string(),
+        format!("\n{}", flat.to_string()),
         r#"
-abc  4  ****
-def
+shark ᗒ  ****
+tiger ᐅ  *******
+whale ᗒ  "#
+    );
+}
 
-abc  3  *"#
+#[test]
+fn categorical_2d() {
+    let builder = Categorical::builder()
+        .add(TwoD::new("whale".to_string(), 4u32), 0)
+        .add(TwoD::new("shark".to_string(), 4u32), 1)
+        .add(TwoD::new("shark".to_string(), 1u32), 3)
+        .add(TwoD::new("tiger".to_string(), 4u32), 1)
+        .add(TwoD::new("tiger".to_string(), 5u32), 3)
+        .add(TwoD::new("tiger".to_string(), 1u32), 3);
+    let flat = builder.render(Render::default());
+    assert_eq!(
+        format!("\n{}", flat.to_string()),
+        r#"
+1 ᗒ shark ᗒ  ****
+4 ᗒ
+1 ᐅ
+4 ᐅ tiger ᐅ  *******
+5 ᐅ
+4 ᗒ whale ᗒ  "#
+    );
+}
+
+#[test]
+fn categorical_3d() {
+    let builder = Categorical::builder()
+        .add(ThreeD::new("whale".to_string(), 4u32, true), 0)
+        .add(ThreeD::new("shark".to_string(), 4u32, false), 1)
+        .add(ThreeD::new("shark".to_string(), 1u32, true), 3)
+        .add(ThreeD::new("tiger".to_string(), 4u32, false), 1)
+        .add(ThreeD::new("tiger".to_string(), 5u32, true), 3)
+        .add(ThreeD::new("tiger".to_string(), 1u32, false), 3);
+    let flat = builder.render(Render::default());
+    assert_eq!(
+        format!("\n{}", flat.to_string()),
+        r#"
+true  ᗒ 1 ᗒ shark ᗒ  ****
+false ᗒ 4 ᗒ
+false ᐅ 1 ᐅ
+false ᐅ 4 ᐅ tiger ᐅ  *******
+true  ᐅ 5 ᐅ
+true  ᗒ 4 ᗒ whale ᗒ  "#
     );
 }
 
@@ -26,7 +72,7 @@ fn histogram() {
         builder = builder.add((i % 10) as f64, i);
     }
 
-    let flat = builder.render(false);
+    let flat = builder.render(Render::default());
     assert_eq!(
         format!("\n{}", flat.to_string()),
         r#"
@@ -38,6 +84,36 @@ fn histogram() {
     );
 }
 
+#[rstest]
+#[case(12)]
+#[case(13)]
+#[case(14)]
+// #[case(15)]
+fn histograsm_squish(#[case] render_width: usize) {
+    let mut builder = Histogram::builder(5);
+
+    for i in 0..10 {
+        builder = builder.add((i % 8) as f64, i);
+    }
+
+    // Make sure one of the bins has a count zero (0).
+    builder = builder.add(9.0, 0);
+
+    let flat = builder.render(Render {
+        render_width,
+        show_values: false,
+    });
+    assert_eq!(
+        format!("\n{}", flat.to_string()),
+        r#"
+[0, 1.8)    **
+[1.8, 3.6)  *
+[3.6, 5.4)  *
+[5.4, 7.2)  **
+[7.2, 9]    "#
+    );
+}
+
 #[test]
 fn histogram_show() {
     let mut builder = Histogram::builder(5);
@@ -46,7 +122,10 @@ fn histogram_show() {
         builder = builder.add((i % 10) as f64, i);
     }
 
-    let flat = builder.render(true);
+    let flat = builder.render(Render {
+        render_width: 120,
+        show_values: true,
+    });
     assert_eq!(
         format!("\n{}", flat.to_string()),
         r#"
@@ -55,5 +134,35 @@ fn histogram_show() {
 [3.6, 5.4)  [ 9] *********
 [5.4, 7.2)  [13] *************
 [7.2, 9]    [17] *****************"#
+    );
+}
+
+#[rstest]
+#[case(17)]
+#[case(18)]
+#[case(19)]
+// #[case(20)]
+fn histogram_show_squish(#[case] render_width: usize) {
+    let mut builder = Histogram::builder(5);
+
+    for i in 0..10 {
+        builder = builder.add((i % 8) as f64, i);
+    }
+
+    // Make sure one of the bins has a count zero (0).
+    builder = builder.add(9.0, 0);
+
+    let flat = builder.render(Render {
+        render_width,
+        show_values: true,
+    });
+    assert_eq!(
+        format!("\n{}", flat.to_string()),
+        r#"
+[0, 1.8)    [18] **
+[1.8, 3.6)  [ 5] *
+[3.6, 5.4)  [ 9] *
+[5.4, 7.2)  [13] **
+[7.2, 9]    [ 0] "#
     );
 }
